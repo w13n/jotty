5ec95bc5a7ab0b943ac5d4a760e8d013e2fa785c
w13n <101300889+w13n@users.noreply.github.com>
2025-08-07 22:28:18 -0400

    added basic model functions

--- src/app.rs
+++ src/app.rs
@@ -1,33 +1,95 @@
 use std::collections::HashMap;
-use time::Date;
+use time::{Date, OffsetDateTime};
 
-struct App {
-    entries: HashMap<Date, Entry>,
+#[derive(Default)]
+pub struct Model {
+    pub journal: Journal,
+    pub should_exit: bool,
 }
 
-struct Entry {
-    events: Vec<Event>,
-    tasks: Vec<Task>,
+impl Model {
+    pub fn new() -> Self {
+        Self {
+            journal: Journal::new(),
+            should_exit: false,
+        }
+    }
 }
 
-struct Event {
-    title: String,
-    importance: Importance,
+#[derive(Default)]
+pub struct Journal(pub HashMap<Date, Entry>);
+
+impl Journal {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    pub fn insert_today(&mut self, entry: Entry) {
+        let today = OffsetDateTime::now_local()
+            .unwrap_or(OffsetDateTime::now_utc())
+            .date();
+        self.0.insert(today, entry);
+    }
 }
 
-enum Importance {
+pub struct Entry {
+    pub events: Vec<Event>,
+    pub tasks: Vec<Task>,
+}
+
+impl Entry {
+    pub fn new() -> Self {
+        Self {
+            events: Vec::new(),
+            tasks: Vec::new(),
+        }
+    }
+
+    pub fn push_event(&mut self, event: Event) {
+        self.events.push(event);
+    }
+
+    pub fn push_task(&mut self, task: Task) {
+        self.tasks.push(task);
+    }
+}
+
+pub struct Event {
+    pub title: String,
+    pub importance: Importance,
+}
+
+impl Event {
+    pub fn new(title: &str, importance: Importance) -> Self {
+        Self {
+            title: title.to_string(),
+            importance,
+        }
+    }
+}
+
+pub enum Importance {
     Low,
     Normal,
     High,
     Extreme,
 }
 
-struct Task {
-    title: String,
-    completion_level: CompletionLevel,
+pub struct Task {
+    pub title: String,
+    pub completion_level: CompletionLevel,
 }
 
-enum CompletionLevel {
+impl Task {
+    pub fn new(title: &str, completion_level: CompletionLevel) -> Self {
+        Self {
+            title: title.to_string(),
+            completion_level,
+        }
+    }
+}
+
+pub enum CompletionLevel {
     None,
     Partial,
     Full,
